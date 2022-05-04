0bd61a3bb1b485e21d099583ed42f746e94c99f5
Loris Leiva <loris.leiva@gmail.com>
2022-05-04 15:49:55 +0100

    Use plan to upload

--- src/CreateNft.js
+++ src/CreateNft.js
@@ -4,15 +4,17 @@ export const CreateNft = () => {
   const { metaplex } = useMetaplex();
 
   const onClick = async () => {
-    const { uri } = await metaplex.nfts().uploadMetadata({ 
-      name: 'My NFT',
-      image: 'https://arweave.net/wu-KmIkjZBUf911v4oSNEJXDAuzvGkn9KwMl38-lZSM',
+    const plan = await metaplex.nfts().planUploadMetadata({
+      name: "My NFT",
+      image: "https://arweave.net/wu-KmIkjZBUf911v4oSNEJXDAuzvGkn9KwMl38-lZSM",
     });
+
+    plan.onChange((step) => console.log(step));
+
+    const { uri } = await plan.execute();
     const { nft } = await metaplex.nfts().createNft({ uri });
     console.log(nft);
   };
 
-  return (
-    <button onClick={onClick}>Create NFT</button>
-  )
-}
+  return <button onClick={onClick}>Create NFT</button>;
+};
